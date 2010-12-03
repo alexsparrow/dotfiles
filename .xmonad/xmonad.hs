bff19b129421e3e453fab5e8a0862f50423f7616
Alex Sparrow <alex@pciccms04.(none)>
2010-12-03 21:27:57 +0100

    Update config
    .emacs points to .alexdot now
    Xmonad has named workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,26 @@
 import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Config.Gnome
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.DynamicLog
+import System.IO
 
 myManageHook :: [ManageHook]
-myManageHook = 
+myManageHook =
     [ resource  =? "Do"   --> doIgnore ]
 
-main = xmonad gnomeConfig
+main = do
+xmproc <- spawnPipe "xmobar"
+xmonad $ gnomeConfig
     { manageHook = manageHook gnomeConfig <+> composeAll myManageHook
 	, modMask = mod4Mask
-         , terminal = "urxvt" }
+        , terminal = "urxvt"
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , workspaces = ["mail", "chat", "web", "code", "physics", "term", "papers", "surf", "misc"]
+--           , normalBorderColor = "#3f3c6d"
+--           , focusedBorderColor = "#4f66ff"
+}
 
