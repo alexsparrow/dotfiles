265a6304bdf48d6c41680db76049473ba18d4218
Alex Sparrow <alex.sparrow@cern.ch>
2011-01-22 19:49:03 +0100

    Add Haskell Mode

--- emacs/alex/my-generic.el
+++ emacs/alex/my-generic.el
@@ -56,3 +56,10 @@
   scroll-preserve-screen-position 1)
 
 (winner-mode 1)
+
+; Load haskell mode
+(load "~/.alexdot/emacs/vendor/haskell-mode-2.8.0/haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)		
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
