d44872d474e2412eb50f33cedaa03bf7eb20a7e1
Alex Sparrow <alex.sparrow@cern.ch>
2011-01-25 00:45:50 +0100

    Add grid selector and spawn rules to my xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,22 +5,38 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.DynamicLog
 import System.IO
 
+import XMonad.Actions.GridSelect
+
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
 myManageHook :: [ManageHook]
 myManageHook =
-    [ resource  =? "Do"   --> doIgnore ]
+    [ resource  =? "Do"   --> doIgnore
+    , className =? "Skype"          --> moveTo "2:chat"
+    , className =? "Evolution"    --> moveTo "1:mail"
+    ]
+    where moveTo = doF . W.shift
 
 main = do
-xmproc <- spawnPipe "xmobar"
-xmonad $ gnomeConfig
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ gnomeConfig
     { manageHook = manageHook gnomeConfig <+> composeAll myManageHook
-	, modMask = mod4Mask
-        , terminal = "urxvt"
-        , logHook = dynamicLogWithPP $ xmobarPP
+    , modMask = mod4Mask
+    , terminal = "urxvt"
+    , logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
-        , workspaces = ["1:mail", "2:chat", "3:web", "4:code", "5:term", "6:write", "7:read", "8:bs", "9:etc"]
+    , workspaces = ["1:mail", "2:chat", "3:web", "4:code", "5:term", "6:write", "7:read", "8:bs", "9:etc"]
+    , keys = newKeys
 --           , normalBorderColor = "#3f3c6d"
 --           , focusedBorderColor = "#4f66ff"
-}
+        }
+
+newKeys x  = M.union (keys defaultConfig x) (M.fromList (myKeys x))
+myKeys conf@(XConfig {XMonad.modMask = modm}) =
+  [
+    ((modm, xK_g), goToSelected defaultGSConfig)
+  ]
 
