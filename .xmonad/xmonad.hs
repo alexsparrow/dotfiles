74dc71d3178735800325d76de19505fa339ef5b1
Alex Sparrow <alex.sparrow@cern.ch>
2011-01-25 18:53:25 +0100

    Pimp xmonad layouts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -10,6 +10,18 @@ import XMonad.Actions.GridSelect
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
+
+import XMonad.Layout
+import XMonad.Layout.Grid
+import XMonad.Layout.IM
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.StackTile
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Tabbed
+
 myManageHook :: [ManageHook]
 myManageHook =
     [ resource  =? "Do"   --> doIgnore
@@ -17,6 +29,29 @@ myManageHook =
     , className =? "Evolution"    --> moveTo "1:mail"
     ]
     where moveTo = doF . W.shift
+
+-- Layouts
+myLayout = avoidStruts $ onWorkspace "2:chat" imLayout $ standardLayouts
+  where
+    -- define the list of standardLayouts
+    standardLayouts = tiled ||| Mirror tiled ||| Full ||| simpleTabbed
+
+    -- notice withIM is acting on it
+    imLayout        = withIM (1/10) skype (standardLayouts)
+
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
+
+    -- The default number of windows in the master pane
+    nmaster = 1
+
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
+
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
+
+    skype = Title "alex_sparrow - Skype™ (Beta)" `Or` Title "Skype™ 2.1 (Beta) for Linux"
 
 main = do
   xmproc <- spawnPipe "xmobar"
@@ -30,6 +65,7 @@ main = do
                         }
     , workspaces = ["1:mail", "2:chat", "3:web", "4:code", "5:term", "6:write", "7:read", "8:bs", "9:etc"]
     , keys = newKeys
+     , layoutHook = myLayout
 --           , normalBorderColor = "#3f3c6d"
 --           , focusedBorderColor = "#4f66ff"
         }
