9b959a2cdf281f13409f270b137030bcdc8984d8
Alex Sparrow <alex.sparrow@cern.ch>
2011-02-01 07:27:50 +0100

    Add some useful keybindings to conkeror

--- conkerorrc/conkeror.js
+++ conkerorrc/conkeror.js
@@ -20,3 +20,27 @@ for (let i = 0; i < 10; ++i) {
 
 
 define_webjump("reddit","http://www.reddit.com/r/%s");
+
+define_key(content_buffer_normal_keymap, "C-c n", "follow-new-buffer");
+
+// copy url with C-c u
+interactive("copy-url",
+        "Copy the current buffer's URL to the clipboard",
+        function(I) {
+            var text = I.window.buffers.current.document.location.href;
+            writeToClipboard(text);
+            I.window.minibuffer.message("copied: " + text);
+        }
+);
+define_key(default_global_keymap, "C-c u", "copy-url");
+
+
+// reload conkerorrc with C-c r
+interactive("reload-config", "reload conkerorrc",
+       function(I) {
+          load_rc();
+          I.window.minibuffer.message("config reloaded");
+       }
+);
+define_key(default_global_keymap, "C-c r", "reload-config");
+
