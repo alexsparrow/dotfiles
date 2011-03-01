974b3141220ec1dfe629988396641aef810d0e66
Alex Sparrow <alex.sparrow@cern.ch>
2011-03-01 23:27:51 +0100

    Mutt updates

--- emacs/alex/my-generic.el
+++ emacs/alex/my-generic.el
@@ -19,6 +19,21 @@
 ;; Trailing whitespace is unnecessary
 (add-hook 'before-save-hook (lambda () (delete-trailing-whitespace)))
 
+;; Make mutt switch to mail-mode
+(add-to-list 'auto-mode-alist '(".*mutt.*" . message-mode))                                                                   
+  (setq mail-header-separator "")                                                                                               
+  (add-hook 'message-mode-hook
+          'turn-on-auto-fill
+          (function
+           (lambda ()
+             (progn
+               (local-unset-key "\C-c\C-c")
+               (define-key message-mode-map "\C-c\C-c" '(lambda ()
+                                                          "save and exit quickly"
+                                                          (interactive)
+                                                          (save-buffer)
+                                                          (server-edit)))))))
+
 (require 'color-theme)
 (color-theme-initialize)
 
