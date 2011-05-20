4a544195fc129c0e94b1f451254d4d5f2061639f
Alex Sparrow <alex.sparrow@cern.ch>
2011-05-20 17:50:21 +0200

    Add .cms command to connect to cmsusr

--- aliases.sh
+++ aliases.sh
@@ -10,3 +10,5 @@ alias lxplus="ssh as1604@lxplus.cern.ch"
 alias root="root -l"
 alias results="cd /home/alex/Research/Results"
 alias alspar="ssh alex@alspar.dyndns.org"
+
+alias .cms='ssh -t as1604@lxplus.cern.ch -L 1088:localhost:1088 "ssh -ND 1088 as1604@cmsusr0"'
