3f0f5f402bb356b59cb3e115f23a036d5855d939
Alex Sparrow <alex.sparrow@cern.ch>
2012-02-05 13:40:17 +0000

    beep

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -12,3 +12,6 @@ alias results="cd /home/alex/Research/Results"
 alias alspar="ssh alex@alspar.dyndns.org"
 
 alias .cms='ssh -t as1604@lxplus.cern.ch -L 1088:localhost:1088 "ssh -ND 1088 as1604@cmsusr0"'
+
+alias -g B=';echo -en "\007"'
+
