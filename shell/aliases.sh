811235c920d8857cced95aca16e6ad160a7c9222
Alex Sparrow <alex.sparrow@cern.ch>
2012-03-19 09:12:50 +0100

    Some shell aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,7 +1,7 @@
 alias ipy=ipython
 alias physlib="source ~/Research/PhysicsLib/setup.sh"
 alias go="source ~/Apps/go/src/env.bash"
-alias ec="emacsclient --alternate-editor emacs -n"
+alias ec="emacsclient -nc"
 
 alias lx05="ssh as1604@lx05.hep.ph.ic.ac.uk"
 alias lx06="ssh as1604@lx06.hep.ph.ic.ac.uk"
@@ -14,4 +14,4 @@ alias alspar="ssh alex@alspar.dyndns.org"
 alias .cms='ssh -t as1604@lxplus.cern.ch -L 1088:localhost:1088 "ssh -ND 1088 as1604@cmsusr0"'
 
 alias -g B=';echo -en "\007"'
-
+alias rtfn='elinks https://www.archlinux.org/feeds/news/'
