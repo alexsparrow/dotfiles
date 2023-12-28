69c66a2a21281c2159be07548604193479ee68ba
Alex Sparrow <alex@alexsparrow.dev>
2023-12-28 12:30:00 +0200

    Remove more cruft

--- my.bash
+++
@@ -1,9 +0,0 @@
-ALEXDOT=$HOME/.alexdot
-
-# PS1
-#PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
-
-# sources
-source $ALEXDOT/bash/git-completion.bash
-source $ALEXDOT/shell/aliases.sh
-source $ALEXDOT/shell/exports.sh
