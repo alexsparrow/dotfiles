26abb278a49bbc160c3e80ab6dbde0efe18d6ab8
Alex Sparrow <alex@alexsparrow.dev>
2023-12-28 13:19:26 +0200

    Add bin

--- home.nix
+++ home.nix
@@ -19,11 +19,8 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = with pkgs; [
+    jq
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -39,12 +36,13 @@
     # '')
   ];
 
-  # environment.systemPackages = [
-  #   ];
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".bin" = {
+      source = ./bin;
+      recursive = true;
+    };
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
