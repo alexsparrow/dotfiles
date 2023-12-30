987ea153bbd222c3387bbddb5704eae356a8c4a8
Alex Sparrow <alex@alexsparrow.dev>
2023-12-30 14:27:19 +0200

    Fixes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -103,12 +103,9 @@ in
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (pkgs.writeShellScriptBin "my-hello" ''
+      echo "Hello, ${config.home.username}!"
+    '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
