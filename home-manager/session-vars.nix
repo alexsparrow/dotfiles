ebf7e3ac7eba6d7b9ee3ecfb7fd6b575b0ed1e2f
Alex Sparrow <alex@alexsparrow.dev>
2023-12-30 15:01:32 +0200

    Move session vars

--- home-manager/session-vars.nix
+++ home-manager/session-vars.nix
@@ -1,21 +1,23 @@
-# Home Manager can also manage your environment variables through
-# 'home.sessionVariables'. If you don't want to manage your shell through Home
-# Manager then you have to manually source 'hm-session-vars.sh' located at
-# either
-#
-#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-#
-# or
-#
-#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-#
-# or
-#
-#  /etc/profiles/per-user/alex/etc/profile.d/hm-session-vars.sh
-#
-home.sessionVariables = {
-EDITOR = "vim";
-NIXOS_OZONE_WL = "1";
-# See: https://github.com/simonmichael/hledger/issues/1033
-LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
-};
+{
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/alex/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "vim";
+    NIXOS_OZONE_WL = "1";
+    # See: https://github.com/simonmichael/hledger/issues/1033
+    LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
+  };
+}
